aedc5ba6d0ab2d73751a1d5a40750e7e6d629359
syahrilr <syahrilramadhan754@gmail.com>
2021-05-26 11:45:58 +0800

    Menambah ViewModel dan Adapter

--- app/src/main/java/syahril/ramadhan/roomdb/MainActivity.kt
+++ app/src/main/java/syahril/ramadhan/roomdb/MainActivity.kt
@@ -2,10 +2,19 @@ package syahril.ramadhan.roomdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var noteViewModel: NoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        noteViewModel = ViewModelProvider(this).get(NoteViewModel::class.java)
+        noteViewModel.getNotes()?.observe(this, Observer {
+            noteAdapter.setNotes(it)
+        })
     }
 }
